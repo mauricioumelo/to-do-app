4c41a19ec72fb75b9ee02939def3dcaa81585bb5
mauricioumelo <mauricioumelo@gmail.com>
2024-11-17 17:57:49 -0300

    refactor: update TaskService with exception handling

--- api/app/Services/TaskService.php
+++ api/app/Services/TaskService.php
@@ -8,10 +8,9 @@ use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rule;
 use Illuminate\Validation\ValidationException;
 
-
 class TaskService
 {
-    function __construct(protected TaskRepositoryInterface $repository) {}
+    public function __construct(protected TaskRepositoryInterface $repository) {}
 
     public function create(array $data)
     {
@@ -27,5 +26,37 @@ class TaskService
         }
 
         return $this->repository->create($data);
+    }
+
+    public function findAll(?string $orderBy = null, ?string $order = null, ?array $filters = [])
+    {
+        return $this->repository->findAll($orderBy, $order, $filters);
+    }
+
+    public function findById(int $id)
+    {
+        return $this->repository->findById($id);
+    }
+
+    public function update(int $id, array $data)
+    {
+        $validator = Validator::make(data: $data, rules: [
+            'title' => 'nullable|string|max:255',
+            'description' => 'nullable|string',
+            'status' => [Rule::enum(TaskStatus::class), 'nullable'],
+        ]);
+
+        if ($validator->fails()) {
+            throw new ValidationException($validator);
+        }
+
+        return $this->repository->update($id, $data);
+    }
+
+    public function delete(int $id)
+    {
+        $this->repository->delete($id);
+
+        return true;
     }
 }
