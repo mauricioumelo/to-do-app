0091a257f19414a7affce3a6a83ecfff4d8be36c
mauricioumelo <mauricioumelo@gmail.com>
2024-11-17 17:56:58 -0300

    feat: add TaskNotFoundException

---
+++ api/app/Exceptions/TaskNotFoundException.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+
+class TaskNotFoundException extends Exception
+{
+    public function render()
+    {
+        return response()->json([
+            'message' => 'Task not found',
+        ], 404);
+    }
+}
