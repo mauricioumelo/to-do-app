e244335c309611178549c42996405fd5ea4a5587
mauricioumelo <mauricioumelo@gmail.com>
2024-11-17 17:54:51 -0300

    refactor: improve task repository and contracts

--- api/app/Repositories/Contracts/TaskRepositoryInterface.php
+++ api/app/Repositories/Contracts/TaskRepositoryInterface.php
@@ -4,8 +4,13 @@ namespace App\Repositories\Contracts;
 
 interface TaskRepositoryInterface
 {
+    public function findAll(string $orderBy, string $order, array $filters);
+
     public function create(array $data);
+
     public function update(int $id, array $data);
+
     public function delete(int $id);
+
     public function findById(int $id);
 }
--- api/app/Repositories/TaskRepository.php
+++ api/app/Repositories/TaskRepository.php
@@ -1,19 +1,58 @@
 <?php
 
-namespace App\Repository;
+namespace App\Repositories;
 
+use App\Exceptions\TaskNotFoundException;
 use App\Models\Task;
 use App\Repositories\Contracts\TaskRepositoryInterface;
 
 class TaskRepository implements TaskRepositoryInterface
 {
     public function __construct(protected Task $model) {}
+
+    public function findAll(?string $orderBy, ?string $order, ?array $filters = [])
+    {
+        return $this->model->when(! empty($filters), function ($query) use ($filters) {
+            $query->where($filters['column'], $filters['value']);
+        })->orderBy($orderBy ?? 'created_at', $order ?? 'desc')->get();
+    }
+
     public function create(array $data)
     {
         return $this->model->create($data);
     }
 
-    public function update(int $id, array $data) {}
-    public function delete(int $id) {}
-    public function findById(int $id) {}
+    public function update(int $id, array $data)
+    {
+        $task = $this->model->where('id', $id)->first();
+
+        if (!$task) {
+            throw new TaskNotFoundException();
+        }
+        $task->update($data);
+
+        return $task;
+    }
+
+    public function delete(int $id)
+    {
+        $task = $this->model->where('id', $id)->first();
+
+        if (!$task) {
+            throw new TaskNotFoundException();
+        }
+        $task->delete();
+
+        return true;
+    }
+
+    public function findById(int $id)
+    {
+        $task = $this->model->where('id', $id)->first();
+
+        if (!$task) {
+            throw new TaskNotFoundException();
+        }
+        return $task;
+    }
 }
